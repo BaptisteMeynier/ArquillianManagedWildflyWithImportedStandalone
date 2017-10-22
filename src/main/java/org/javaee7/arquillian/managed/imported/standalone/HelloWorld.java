3196419d43938047163e5e1ff0b9e767bd4a5cbb
Baptiste Meynier <baptiste.meynier@gmail.com>
2017-10-22 19:49:50 +0200

    Managed wildfly with Arquillian,  allow to load a customed standalone with played cli command

---
+++ src/main/java/org/javaee7/arquillian/managed/imported/standalone/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.javaee7.arquillian.managed.imported.standalone;
+
+public interface HelloWorld {
+
+	String print();
+	
+}
---
+++ src/test/java/org/javaee7/arquillian/managed/imported/standalone/ArquillianSimpleTest.java
@@ -0,0 +1,35 @@
+package org.javaee7.arquillian.managed.imported.standalone;
+
+import javax.inject.Inject;
+
+import org.javaee7.arquillian.managed.imported.standalone.HelloWorld;
+import org.javaee7.arquillian.managed.imported.standalone.HelloWorldImpl;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ArquillianSimpleTest {
+	
+	@Inject
+	private HelloWorld helloWorld;
+	
+	@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(HelloWorld.class)
+            .addClass(HelloWorldImpl.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void should_create_greeting() {
+        Assert.assertEquals("Hello world",helloWorld.print());
+    }
+
+}
